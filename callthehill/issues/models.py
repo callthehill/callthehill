457ecd88184e521686ecca931bda14f699d84e56
Zach Zundel <zach.zundel@utah.edu>
2017-01-25 12:44:24 -0700

    Added models to Issue app
    Models added:
     - Issue
     - Tag

--- callthehill/issues/models.py
+++ callthehill/issues/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Issue(models.Model):
+    """
+    An Issue about which you call a Legislator
+    Tied to one, and only one, primary Legislation
+    Legislations associated with the primary Legislation are used to determine calls
+    """
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    legislation = models.ForeignKey("legislation.Legislation")
+    tags = models.ManyToManyField('Tag')
+
+class Tag(models.Model):
+    """
+    A Tag which can be added to an Issue
+    """
+    tag = models.CharField(max_length=10)
+    description = models.CharField(max_length=140)
