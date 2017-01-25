5c596ac5e0aec3da0401e2818e0ed0d97763db88
Zach Zundel <zach.zundel@utah.edu>
2017-01-25 12:15:26 -0700

    Added models to legislation app
    Models added:
     - Legislator
     - Honorific
     - Pronouns
     - Party
     - Body
     - Legislation

--- callthehill/legislation/models.py
+++ callthehill/legislation/models.py
@@ -1,3 +1,77 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+class Legislator(models.Model):
+    """
+    A specific legislator
+    """
+    gender_options = (('F', "Feminine"),
+                      ('N', "Nonbinary"),
+                      ('M', "Masculine"))
+    invalid_phone = "Phone number must be in the format +999999999. Up to 15 digits allowed."
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message=invalid_phone)
+
+    family_name = models.CharField(max_length=30)
+    given_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, choices=gender_options)
+    pronouns = models.ForeignKey("Pronouns")
+    party = models.CharField(max_length=3)
+    body = models.ForeignKey("Body")
+    phone_number = models.CharField(max_length=16, validators=[phone_regex], blank=True)
+
+class Honorific(models.Model):
+    """
+    An honorific set used by Legislators in a Body
+    """
+    neutral = models.CharField(max_length=30)
+    masculine = models.CharField(max_length=30)
+    feminine = models.CharField(max_length=30)
+
+class Pronouns(models.Model):
+    """
+    The set of pronouns used by a Legislator
+    """
+    sub = models.CharField(max_length=30)
+    obj = models.CharField(max_length=30)
+    own = models.CharField(max_length=30)
+    ref = models.CharField(max_lenght=30)
+
+class Party(models.Model):
+    """
+    A political party
+    """
+    name = models.CharField(max_length=30)
+    abbreviation = models.CharField(max_length=3)
+
+class Body(models.Model):
+    """
+    A legislative body
+    """
+    name = models.TextField()
+    location = models.TextField()
+    honorific = models.ForeignKey("Honorific")
+    controlling_party = models.ForeignKey("Party")
+
+class Vote(models.Model):
+    """
+    A single Legislator"s vote on a Legislation
+    """
+    vote_options = (('Y', "Yea"),
+                    ('N', "Nay"),
+                    ('A', "Abstain"))
+
+    legislator = models.ForeignKey("Legislator")
+    vote = models.CharField(max_length=1, choices=vote_options)
+
+class Legislation(models.Model):
+    """
+    A piece of Legislation sponsored by Legislators in a Body
+    """
+    name = models.TextField(blank=False)
+    reference = models.URLField()
+    sponsors = models.ManyToManyField("Legislator")
+    body = models.ForeignKey("Body")
+    votes = models.ManyToMany("Vote")
+
+    related_legislation = models.ManyToManyField("self")
