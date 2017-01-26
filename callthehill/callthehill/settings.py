a01b8948af89ea42badb70c11128a33ca5cc81c9
Zach Zundel <zach.zundel@utah.edu>
2017-01-25 20:39:37 -0700

    Added applications to settings

--- callthehill/callthehill/settings.py
+++ callthehill/callthehill/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'activist',
+    'issues',
+    'legislation',
+    'phonebank',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
