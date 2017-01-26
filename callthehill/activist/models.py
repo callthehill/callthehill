b36200f9b23368f63086c540bb2bc477d49022b7
Zach Zundel <zach.zundel@utah.edu>
2017-01-25 23:25:31 -0700

    Allowed issues to be blank

--- callthehill/activist/models.py
+++ callthehill/activist/models.py
@@ -13,7 +13,7 @@ class Activist(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    interests = models.ManyToManyField('issues.Tag')
+    interests = models.ManyToManyField('issues.Tag', blank=True)
     name = models.CharField(max_length=50, blank=True)
     location = models.CharField(max_length=100, blank=True)
     zip_code = models.CharField(max_length=10, validators=[zip_validator])
