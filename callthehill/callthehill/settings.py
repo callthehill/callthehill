b7edd14d8dc91281a3b43ae15d183a9de553327c
Zach Zundel <zach.zundel@utah.edu>
2017-01-25 21:44:02 -0700

    Updated settings with new Django

--- callthehill/callthehill/settings.py
+++ callthehill/callthehill/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'activist',
-    'issues',
-    'legislation',
-    'phonebank',
+    'activist.apps.ActivistConfig',
+    'issues.apps.IssuesConfig',
+    'legislation.apps.LegislationConfig',
+    'phonebank.apps.PhonebankConfig',
     'rest_framework',
 ]
 
