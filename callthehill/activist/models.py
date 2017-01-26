f808f94c9911e9281974e7d79d296d1f6cbd3fea
Zach Zundel <zach.zundel@utah.edu>
2017-01-25 21:44:18 -0700

    Added user uuid

--- callthehill/activist/models.py
+++ callthehill/activist/models.py
@@ -1,15 +1,17 @@
+import uuid
 from django.conf import settings
 from django.core.validators import RegexValidator
 from django.db import models
 
 class Activist(models.Model):
     """
-    An Activist is a user on CallTheHill
+    An Activist is a user on Call the Hill
     """
     invalid_zip = "The ZIP code you entered is invalid"
 
     zip_validator = RegexValidator(regex=r'^\d{5}(?:[-\s]\d{4})?$', message=invalid_zip)
 
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     interests = models.ManyToMany('issues.Tag')
     name = models.CharField(max_length=50, blank=True)
