eaa1fb705d1834416dce44452543d1f93dc83a55
Zach Zundel <zach.zundel@utah.edu>
2017-01-29 13:48:09 -0700

    Removed card serializers

--- callthehill/activist/serializers.py
+++ callthehill/activist/serializers.py
@@ -1,15 +1,6 @@
 from activist.models import Activist
 from rest_framework import serializers
 
-class IssueCardSerializer(serializers.Serializer):
-    pass
-
-class NewLegislationCardSerializer(serializers.Serializer):
-    pass
-
-class UpdateCardSerializer(serializers.Serializer):
-    pass
-
 class ActivistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Activist
